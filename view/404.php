bcbf2eeab8f2d085838039b26d5b8397f581d8dd
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-12 01:46:31 +0200

    Add 404 controller

---
+++ view/404.php
@@ -0,0 +1,7 @@
+    <!-- Begin page content -->
+    <div class="container">
+      <div class="page-header">
+        <h1>Erreur 404 !</h1>
+      </div>
+      <p class="lead">Il n'y a rien sur cette page, ce n'est pas grave, tu survivras !</p>
+    </div>
