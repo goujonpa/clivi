af4230865ed1e0e7e45e50124de18568695460d4
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-11 22:08:46 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>CLIVI: The Clinique</title>
+</head>
+<body text="#000000" bgcolor="#FFFFFF" link="#0000EE" vlink="#551A8B" alink="#FF0000">
+    <hr WIDTH="100%">
+    <h3>
+        <center>
+        <font color="#CC0000">This project is not yet implemented !</font>
+        </h3>
+    </center>
+    <hr WIDTH="100%">
+</body>
+</html>
