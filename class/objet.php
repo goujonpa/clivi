dbc672c4f17eb3d8f67d1d0d9d776f28dcb0f86c
clement <cmercier@etu.utc.fr>
2014-06-12 03:20:43 +0200

    getAll

--- class/objet.php
+++ class/objet.php
@@ -14,7 +14,18 @@ class Objet
     public static function getAll()
     {
     	// TODO return list of objects Client
-        echo "toto:".get_called_class();
-    	return Array();
+        $dbName = lcfirst(get_called_class());
+        $bdd = new Db();
+        $result = Array();
+
+        $requete_prepare = $bdd->db->prepare("SELECT * FROM :table"); // on prépare notre requête
+        $requete_prepare->execute(array( 'table' => $dbName ));
+        while($ligne = $requete_prepare->fetch(PDO::FETCH_ASSOC)) {
+            $temp = new get_called_class()();
+            $temp->fromDb($ligne);
+            $result[] = $temp;
+        }
+
+    	return $result;
     }
 }
