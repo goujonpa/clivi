0cd9eceb364b93b7d79509ce2f05690fb8c3ce70
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-12 03:27:22 +0200

    try to change db config

--- class/db.php
+++ class/db.php
@@ -8,7 +8,7 @@ class Db
 	{
 		try
 		{
-			$this->db = new PDO('pgsql:host=tuxa.sme.utc;dbname=nf17p110', 'nf17p110', 'gXYc4mPg');
+			$this->db = new PDO('pgsql:host=tuxa.sme.utc;dbname=nf17p110', 'dbnf17p110', 'gXYc4mPg');
 		}
 		catch (Exception $e)
 		{
