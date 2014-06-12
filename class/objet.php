b8f8c28b124945675bd61d691576f97d60cbb32b
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-12 03:35:24 +0200

    debug

--- class/objet.php
+++ class/objet.php
@@ -14,15 +14,12 @@ class Objet
     public static function getAll()
     {
         $className = get_called_class();
-        echo $className;
         $dbName = lcfirst($className);
         $bdd = new Db();
         $result = Array();
 
-        echo $dbName . "<br />";
-
-        $requete_prepare = $bdd->db->prepare("SELECT * FROM :table"); // on prépare notre requête
-        $requete_prepare->execute(array( 'table' => $dbName ));
+        $requete_prepare = $bdd->db->prepare("SELECT * FROM client"); // on prépare notre requête
+        $requete_prepare->execute(); //array( 'table' => $dbName ));
 
         while($ligne = $requete_prepare->fetch(PDO::FETCH_ASSOC)) {
             echo print_r($ligne, true) . "<br />";
