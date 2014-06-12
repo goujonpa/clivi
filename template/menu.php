d2611e8b1137d6fd286edf8788f604f5487f5d7a
cortylal <alexcortyl@gmail.com>
2014-06-12 02:20:38 +0200

    completing menu.php

--- template/menu.php
+++ template/menu.php
@@ -1,33 +1,60 @@
     <!-- Fixed navbar -->
     <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Clinique Vétérinaire</a>
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">Clinique Vétérinaire</a>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Personne <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li class="dropdown-header">Personnel</li>
+                            <li><a href="#persList">Liste</a></li>
+                            <li><a href="#persAdd">Ajouter</a></li>
+                            <li class="divider"></li>
+                            <li class="dropdown-header">Client</li>
+                            <li><a href="#clientList">Liste</a></li>
+                            <li><a href="#clientAdd">Ajouter</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Animaux<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#persList">Liste</a></li>
+                            <li><a href="#persAdd">Ajouter</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Rendez-vous<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#persList">Liste</a></li>
+                            <li><a href="#persAdd">Ajouter</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Ordonnence<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#persList">Liste</a></li>
+                            <li><a href="#persAdd">Ajouter</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Facture<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#persList">Liste</a></li>
+                            <li><a href="#persAdd">Ajouter</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#contact">Statistiques</a></li>
+                    <li class="active"><a href="#">Home</a></li>
+                </ul>
+            </div><!--/.nav-collapse -->
         </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Personne <b class="caret"></b></a>
-              <ul class="dropdown-menu">
-                <li class="dropdown-header">Personnel</li>
-                <li><a href="#persList">Liste</a></li>
-                <li><a href="#persAdd">Ajouter</a></li>
-                <li class="divider"></li>
-                <li class="dropdown-header">Client</li>
-                <li><a href="#clientList">Liste</a></li>
-                <li><a href="#clientAdd">Ajouter</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
     </div>
