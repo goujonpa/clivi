51995a41ef3a274bf603bb81c4f9ba9391adf662
clement <clemmercier45@gmail.com>
2014-06-18 13:01:22 +0200

    try

--- class/race.php
+++ class/race.php
@@ -2,7 +2,7 @@
 
 class Race extends Objet
 {
-    protected $_primaryAttr = "nom";
+    public $_primaryAttr = "nom";
     protected $nom;
     protected $espece;
     protected $prix_intervention;
