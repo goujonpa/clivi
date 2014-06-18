35e129bce763937e15a8c74503a2c98e08dd6a3c
Matthieu Guffroy <mattgu74@gmail.com>
2014-06-18 17:30:43 +0200

    repair BoolField

--- class/boolfield.php
+++ class/boolfield.php
@@ -12,11 +12,13 @@ class BoolField extends Field
 
     public function validate()
     {
-        if (isset($_POST[$this->name]))
-            if ($_POST[$this->name] == 'on')
-                $this->value = true;
-            else
-                $this->value = false;
+        if (isset($_POST[$this->name])) 
+        {
+            $this->value = true;
+        }
+        else {
+            $this->value = false;
+        }
         return true;
     }
 }
