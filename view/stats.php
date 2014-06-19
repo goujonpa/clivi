de1cd8ff81281265d09f6686d2127f5b29a013bd
cortylal <alexcortyl@gmail.com>
2014-06-19 02:18:17 +0200

    stats

--- view/stats.php
+++ view/stats.php
@@ -12,15 +12,15 @@
         </thead>
         <thead>
                 <th>Nombre d'employé' : </th>
-                <th><?php $tmp = $stats->nbEmploye(); $nb_formated = number_format($tmp["nb"], 2, ',', ' '); echo $nb_formated; ?></th>
+                <th><?php $tmp = $stats->nbEmploye(); $nb_formated = number_format($tmp["nb"]); echo $nb_formated; ?></th>
         </thead>
         <thead>
                 <th>Nombre de vétérinaire' : </th>
-                <th><?php $tmp = $stats->nbVeterinaire(); $nb_formated = number_format($tmp["nb"], 2, ',', ' '); echo $nb_formated; ?></th>
+                <th><?php $tmp = $stats->nbVeterinaire(); $nb_formated = number_format($tmp["nb"]); echo $nb_formated; ?></th>
         </thead>
         <thead>
                 <th>Nombre d'animaux : </th>
-                <th><?php $tmp = $stats->nbAnimaux(); $nb_formated = number_format($tmp["nb"], 2, ',', ' '); echo $nb_formated; ?></th>
+                <th><?php $tmp = $stats->nbAnimaux(); $nb_formated = number_format($tmp["nb"]); echo $nb_formated; ?></th>
         </thead>
         <thead>
                 <th>Nombre moyen d'animal par client : </th>
