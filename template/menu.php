0ce27fba143bde68fff12c02a5309f011da17296
goujonpa <goujonpa@gmail.com>
2014-06-19 03:30:35 +0200

    logo mouahahahahahaha

--- template/menu.php
+++ template/menu.php
@@ -8,7 +8,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="http://tuxa.sme.utc/~nf17p110/index.php">Clinique Vétérinaire</a>
+                <a class="navbar-brand" href="http://tuxa.sme.utc/~nf17p110/index.php"><img src="img/logo.png"/>Clinique Vétérinaire</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
