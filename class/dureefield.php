a4b308e4af8c3f4defbb5ab895bedbf3c233b6a1
goujonpa <goujonpa@gmail.com>
2014-06-19 02:41:02 +0200

    bonjour dureefield

---
+++ class/dureefield.php
@@ -0,0 +1,30 @@
+<?php
+
+class DureeField extends Field
+{
+    public function html() {
+        // Todo, if isset($_POST[$this->name]) && !$this->validate() => Show a error msg explanation for this line.
+        $html = '<div class="form-group">
+                    <label for="form'.$this->name.'">'.$this->label.'</label>
+                    <input type="number" class="form-control" name="'.$this->name.'" value="'.$this->value.'"> 
+                </div>';
+        return $html; 
+    }
+
+    public function validate()
+    {
+        $this->value = $_POST[$this->name];
+        if (this->value > -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public function show() {
+
+        $heures = (int)$this->value / 60;
+        $minutes = (int)$this->value % 60;
+        $html = $heures.'h '.$minutes.'min';
+        return $html; 
+    }
+}
