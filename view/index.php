7cf570404bbbcffdad88af4112ada86b6464d6e2
goujonpa <goujonpa@gmail.com>
2014-06-19 03:28:06 +0200

    logo mouahahahahahaha

--- view/index.php
+++ view/index.php
@@ -1,6 +1,7 @@
     <!-- Begin page content -->
     <div class="container">
       <div class="page-header">
+        <img src="img/logo.png"/>
         <h1>Clinique Vétérinaire</h1>
       </div>
       <p class="lead">Bienvenue sur l'outil de gestion de la clinique.</p>
